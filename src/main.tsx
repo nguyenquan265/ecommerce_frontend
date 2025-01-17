8f5f80da31f7e437f78e251f6b973ac04e736279
kimochiee <90102971+kimochiee@users.noreply.github.com>
2025-01-17 17:45:03 +0700

    add theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from './providers/themeProvider.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,8 +17,10 @@ const queryClient = new QueryClient({
 })
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools />
-  </QueryClientProvider>
+  <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </ThemeProvider>
 )
