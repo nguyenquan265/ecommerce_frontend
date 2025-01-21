759e22ec60fbc3c3f415077758a20c5ed6420bb3
kimochiee <90102971+kimochiee@users.noreply.github.com>
2025-01-21 13:54:45 +0700

    remove unuse query

--- src/apis/categoryApi.ts
+++ src/apis/categoryApi.ts
@@ -17,19 +17,3 @@ export const useGetAllCategories = () => {
 
   return { categories, isLoading }
 }
-
-export const useGetCategory = (categoryId: string = '') => {
-  const createGetCategoryRequest = async (): Promise<Category> => {
-    const res = await authorizedAxios.get(`/categories/${categoryId}`)
-
-    return res.data.category
-  }
-
-  const { data: category, isLoading } = useQuery({
-    queryKey: ['category', { categoryId }],
-    queryFn: createGetCategoryRequest,
-    enabled: categoryId === 'new' ? false : !!categoryId
-  })
-
-  return { category, isLoading }
-}
