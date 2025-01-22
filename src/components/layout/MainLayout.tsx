a46d600c21e2ec1596a790362e6dcfa47c4eb8b4
kimochiee <90102971+kimochiee@users.noreply.github.com>
2025-01-22 18:37:09 +0700

    remove AuthProvider

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,25 +5,22 @@ import { SidebarProvider } from '../ui/sidebar'
 import Footer from '../shared/Footer'
 import Header from '../shared/Header'
 import MobileSidebar from '../shared/MobileSidebar'
-import AuthProvider from '@/providers/AuthProvider'
 import { UserProvider } from '@/contexts/UserContext'
 
 const MainLayout = () => {
   return (
     <UserProvider>
-      <AuthProvider>
-        <SidebarProvider defaultOpen={false}>
-          <MobileSidebar />
+      <SidebarProvider defaultOpen={false}>
+        <MobileSidebar />
 
-          <div className='min-h-screen w-full bg-background'>
-            <Header />
+        <div className='min-h-screen w-full bg-background'>
+          <Header />
 
-            <Outlet />
+          <Outlet />
 
-            <Footer />
-          </div>
-        </SidebarProvider>
-      </AuthProvider>
+          <Footer />
+        </div>
+      </SidebarProvider>
     </UserProvider>
   )
 }
