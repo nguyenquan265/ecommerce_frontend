e939b4f607dbcca8bfc7d42a0d9be749f830cbf8
kimochiee <90102971+kimochiee@users.noreply.github.com>
2025-03-18 09:31:55 +0700

    remove theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx'
 import { Toaster } from '@/components/ui/sonner'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-// import { ThemeProvider } from './providers/ThemeProvider.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,11 +17,9 @@ const queryClient = new QueryClient({
 })
 
 createRoot(document.getElementById('root')!).render(
-  // <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
   <QueryClientProvider client={queryClient}>
     <App />
     <Toaster />
     <ReactQueryDevtools />
   </QueryClientProvider>
-  // </ThemeProvider>
 )
